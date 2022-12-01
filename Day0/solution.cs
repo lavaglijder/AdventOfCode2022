a6abb75e7eb0a1769df2a67262144aadf47a3da1
Sam <ijsglijder@gmail.com>
2022-12-01 14:09:03 +0100

    Project base

---
+++ Day0/solution.cs
@@ -0,0 +1,25 @@
+﻿class solution
+{
+    static void Main()
+    {
+        string currentDirectory = Environment.CurrentDirectory;
+        string? projectDirectory = Directory.GetParent(currentDirectory)?.Parent?.Parent?.FullName;
+        if (projectDirectory is null) return;
+        string input = System.IO.File.ReadAllText(projectDirectory + "/input.txt");
+        
+        Console.Write(input);
+        
+        Console.WriteLine("\n\nSolution 1 answer: " + solution_1());
+        Console.WriteLine("\nSolution 2 answer: " + solution_2());
+    }
+
+    static object solution_1()
+    {
+        return "none";
+    }
+
+    static object solution_2()
+    {
+        return "none";
+    }
+}
